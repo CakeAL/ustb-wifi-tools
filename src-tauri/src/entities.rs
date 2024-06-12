dbfda0aaa134dc2a00a784f4b5e9648554a457a2
CakeAL <1742507190@qq.com>
2024-06-12 22:37:48 +0800

    ✨ feat(requests): 添加了get_month_pay，可以获取并解析扣费账单

--- src-tauri/src/entities.rs
+++ src-tauri/src/entities.rs
@@ -2,22 +2,38 @@ use std::sync::Mutex;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Flow {
-    pub v4: f64,
-    pub v6: f64,
-}
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct Flow {
+//     pub v4: f64,
+//     pub v6: f64,
+// }
+
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct UserFlow {
+//     pub result: i32,
+//     pub data: Flow,
+// }
+
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct GetUserFlowFailed {
+//     pub result: i32,
+//     pub msg: String,
+// }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct UserFlow {
-    pub result: i32,
-    pub data: Flow,
+pub struct MonthPayInfo {
+    pub year_cost: f64,
+    pub year_used_duration: u32,
+    pub year_used_flow: f64,
+    pub monthly_data: Vec<MonthlyData>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct GetUserFlowFailed {
-    pub result: i32,
-    pub msg: String,
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct MonthlyData {
+    pub month: u8,
+    pub month_cost: f64,
+    pub month_used_flow: f64,
+    pub month_used_duration: u32,
 }
 
 #[derive(Debug)]
--- src-tauri/src/requests.rs
+++ src-tauri/src/requests.rs
@@ -1,16 +1,20 @@
+use std::f64;
+
 use anyhow::{anyhow, Result};
 use regex::Regex;
 use reqwest::{header::SET_COOKIE, Client};
+use scraper::{Html, Selector};
 use serde_json::Value;
+
+use crate::entities::{MonthPayInfo, MonthlyData};
 
 // Ciallo～(∠・ω< )⌒☆
 pub async fn get_load_user_flow(account: &str) -> Result<Value> {
-    let client = Client::new();
     let url = format!(
         "http://202.204.48.66:801/eportal/portal/visitor/loadUserFlow?account={}",
         account
     );
-    let response = client.get(url).send().await?.text().await?;
+    let response = Client::new().get(url).send().await?.text().await?;
     let re = Regex::new(r"jsonpReturn\((.*)\);")?;
     let json_str = re
         .captures(&response)
@@ -59,12 +63,9 @@ pub async fn get_jsessionid(account: &str, password: &str) -> Result<String> {
     }
 }
 
-pub async fn get_refresh_account(
-    session_id: &str,
-) -> Result<Option<String>> {
+pub async fn get_refresh_account(session_id: &str) -> Result<Option<String>> {
     let url = "http://202.204.60.7:8080/refreshaccount";
-    let client = Client::new();
-    let response = client
+    let response = Client::new()
         .get(url)
         .header("Cookie", format!("JSESSIONID={}", session_id))
         .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
@@ -75,48 +76,112 @@ pub async fn get_refresh_account(
         .text()
         .await?;
     // println!("{response}");
-    if response.contains("<html>") {
+    if response.contains("nav_login") {
         return Ok(None); // Cookie无效，没有获取到account信息
     }
     Ok(Some(response))
 }
 
+pub async fn get_month_pay(session_id: &str, year: u16) -> Result<Option<Value>> {
+    let url = "http://202.204.60.7:8080/MonthPayAction.action";
+    let response = Client::new()
+        .post(url)
+        .header("Cookie", format!("JSESSIONID={}", session_id))
+        .form(&[("type", 1), ("year", year)])
+        .send()
+        .await?
+        .text()
+        .await?;
+    // println!("{response}");
+    if response.contains("nav_login") {
+        return Ok(None); // Cookie无效，没有获取到account信息
+    }
+    let parsed_html = Html::parse_document(&response);
+    let redtext_selector = Selector::parse(".redtext").unwrap();
+    let redtexts = parsed_html
+        .select(&redtext_selector)
+        .flat_map(|ele| ele.text().collect::<Vec<&str>>())
+        .collect::<Vec<&str>>();
+    // println!("{:?}", redtexts); // ["0.00", "0.95", "616323", "330284.323"]
+    let monthly_data_selector = Selector::parse(".table4 > tbody > tr > td").unwrap();
+    let monthly_data_text = parsed_html
+        .select(&monthly_data_selector)
+        .flat_map(|ele| ele.text().collect::<Vec<&str>>());
+    // 不是，哥们，你这网页数据存这么多\n, \t干什么啊
+    let mut data_index = 0;
+    let mut monthly_datas = vec![];
+    let mut month_data = MonthlyData {
+        month: 0,
+        month_cost: 0.0,
+        month_used_flow: 0.0,
+        month_used_duration: 0,
+    };
+    for data in monthly_data_text {
+        // println!("{:?}", data);
+        // 依次匹配每行字符串，到7的时候设置为-1，然后+1变成0，说明再读取就到下一行了
+        match data_index {
+            0 => month_data.month = data[5..7].parse::<u8>()?,
+            4 => month_data.month_cost = data.trim().parse::<f64>()?,
+            5 => month_data.month_used_duration = data.trim().parse::<u32>()?,
+            6 => month_data.month_used_flow = data.trim().parse::<f64>()?,
+            7 => {
+                data_index = -1;
+                // println!("{:?}", month_data);
+                monthly_datas.push(month_data);
+            }
+            _ => (),
+        }
+        data_index += 1;
+    }
+    // println!("{:?}", monthly_datas);
+
+    Ok(Some(serde_json::json!(MonthPayInfo {
+        year_cost: redtexts.get(1).unwrap().parse()?,
+        year_used_duration: redtexts.get(2).unwrap().parse()?,
+        year_used_flow: redtexts.get(3).unwrap().parse()?,
+        monthly_data: monthly_datas,
+    })))
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::entities::{GetUserFlowFailed, UserFlow};
-
-    #[warn(unused_imports)]
-    use super::get_jsessionid;
-    use super::get_load_user_flow;
-    #[warn(unused_imports)]
-    use super::get_refresh_account;
+    // use crate::entities::{GetUserFlowFailed, UserFlow};
+    use super::*;
 
     #[tokio::test]
     async fn test_get_load_user_flow() {
         let account = "U202141234".to_string();
         let res = get_load_user_flow(&account).await.unwrap();
         println!("{:?}", res);
-        if let Ok(user_flow) = serde_json::from_value::<UserFlow>(res.clone()) {
-            println!("{:?}", user_flow);
-        } else if let Ok(get_failed) = serde_json::from_value::<GetUserFlowFailed>(res) {
-            println!("Error: {}", get_failed.msg);
-        } else {
-            println!("Error: 其他未知原因");
-        }
+        // if let Ok(user_flow) = serde_json::from_value::<UserFlow>(res.clone()) {
+        //     println!("{:?}", user_flow);
+        // } else if let Ok(get_failed) = serde_json::from_value::<GetUserFlowFailed>(res) {
+        //     println!("Error: {}", get_failed.msg);
+        // } else {
+        //     println!("Error: 其他未知原因");
+        // }
     }
 
     #[tokio::test]
     async fn test_get_jsessionid() {
-        let account = "stu_id".to_string();
-        let password = "md5_password".to_string();
-        let res = get_jsessionid(&account, &password).await;
+        let account = "stu_id";
+        let password = "md5_password";
+        let res = get_jsessionid(account, password).await;
         println!("{:?}", res);
     }
 
     #[tokio::test]
     async fn test_get_refresh_account() {
-        let session_id = "session_id".to_string();
-        let res = get_refresh_account(&session_id).await;
+        let session_id = "session_id";
+        let res = get_refresh_account(session_id).await;
         println!("{:?}", res);
+    }
+
+    #[tokio::test]
+    async fn test_get_month_pay() {
+        let session_id = "session_id";
+        let year = 2024u16;
+        let res = get_month_pay(session_id, year).await;
+        dbg!(res);
     }
 }
