7a050359305cd86e3ce9a44b9b4daab9c5a706a7
CakeAL <1742507190@qq.com>
2024-12-25 16:18:18 +0800

    🐞 fix(store): Top-level await

--- src/store.ts
+++ src/store.ts
@@ -2,21 +2,26 @@ import { invoke } from "@tauri-apps/api/core";
 import { reactive } from "vue";
 
 const getCurUserName = async (): Promise<string> => {
-    return await invoke("get_current_user_name");
-}
+  return await invoke("get_current_user_name");
+};
 
 const setCurUserName = async () => {
-    await invoke("set_current_user_name", {userName: store.userName});  
+  await invoke("set_current_user_name", { userName: store.userName });
+};
+
+async function initializeStore() {
+  const userName = await getCurUserName();
+  return reactive({
+    userName,
+    setUserName(userName: string) {
+      this.userName = userName;
+      setCurUserName();
+    },
+    clearUserName() {
+      this.userName = "";
+      setCurUserName();
+    },
+  });
 }
 
-export const store = reactive({
-  userName: await getCurUserName(),
-  setUserName(userName: string) {
-    this.userName = userName;
-    setCurUserName();
-  },
-  clearUserName() {
-    this.userName = "";
-    setCurUserName();
-  }
-});
+export const store = await initializeStore();
